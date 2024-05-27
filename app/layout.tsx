8d472506f693bc89e069a397fcd4ac73f7e2d7ac
musasu <nekomaobusiness@gmail.com>
2024-05-27 16:16:48 -0400

    updates

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import type { Metadata } from "next";
+import '@styles/globals.css';
+
+
+export const metadata = {
+    title: "Promptopia",
+    description: "Discover & Share AI Prompts"
+}
+
+export default function RootLayout({
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang='en'>
+        <body>
+        <div className='main'>
+            <div className='gradient'/>
+        </div>
+        <main className="app">
+            {children}
+        </main>
+        </body>
+        </html>
+    )
+};
