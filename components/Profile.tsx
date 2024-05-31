8f98ce3f1d800c7063c740d89019fd12a42a2a9f
musasu <nekomaobusiness@gmail.com>
2024-05-31 01:55:13 -0400

    add view other user's profile

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -1,15 +1,24 @@
 import PromptCard from "./PromptCard";
+import { PromptType } from "@models/prompt";
 
-const Profile = ({ name, desc, data, handleEdit, handleDelete }) => {
+export interface ProfileProps{
+  name: string;
+  desc: string;
+  data: PromptType[];
+  handleEdit?:  (post:PromptType) => void;
+  handleDelete?:   (post:PromptType) => void;
+}
+
+const Profile: React.FC<ProfileProps> = ({ name, desc, data, handleEdit, handleDelete }) => {
   return (
     <section className='w-full'>
       <h1 className='head_text text-left'>
-        <span className='blue_gradient'>{name} Profile</span>
+        <span className='blue_gradient'>{name}{"'s"} Profile</span>
       </h1>
       <p className='desc text-left'>{desc}</p>
 
       <div className='mt-10 prompt_layout'>
-        {data.map((post) => (
+        {data.map((post:PromptType) => (
           <PromptCard
             key={post._id}
             post={post}
