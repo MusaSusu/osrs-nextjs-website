e273d8d2862fe238c2cce58f93dc2d14aff67285
musasu <nekomaobusiness@gmail.com>
2024-05-30 20:06:27 -0400

    code style

--- app/create-prompt/page.tsx
+++ app/create-prompt/page.tsx
@@ -1,55 +1,59 @@
-'use client'
+"use client";
 
-import { useState } from 'react';
-import { useSession } from 'next-auth/react';
-import { useRouter } from 'next/navigation';
+import { useState } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
-import { Form, FormProps} from '@components/Form';
+import { Form, FormProps } from "@components/Form";
 
-export interface PostState{
-    prompt: string,
-    tag: string
+export interface PostState {
+  prompt: string;
+  tag: string;
 }
 
 const CreatePrompt = () => {
-    const { data: session } = useSession();
-    const [submitting, setSubmitting] = useState(false);
-    const [post, setPost] = useState<PostState>({
-        prompt: "",
-        tag: " ",
-    });
+  const router = useRouter();
+  const { data: session } = useSession();
+  const [submitting, setSubmitting] = useState(false);
+  const [post, setPost] = useState<PostState>({
+    prompt: "",
+    tag: " ",
+  });
 
-    const createPrompt = async (e:React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-        e.preventDefault();
-        setSubmitting(true);
-        console.log(session?.user.id)
-        try {
-            const response = await fetch("/api/prompt/new", {
-              method: "POST",
-              body: JSON.stringify({
-                prompt: post.prompt,
-                userId: session?.user.id,
-                tag: post.tag,
-              }),
-            });
-            if (response.ok) {
-            }
-          } catch (error) {
-            console.log(error);
-          } finally {
-            setSubmitting(false);
-          }
+  const createPrompt = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+    setSubmitting(true);
+    console.log(session?.user.id);
+    try {
+      const response = await fetch("/api/prompt/new", {
+        method: "POST",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag,
+        }),
+      });
+      if (response.ok) {
+        router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSubmitting(false);
     }
+  };
 
-    return (
-        <Form
-        type = "Create"
-        post = {post}
-        setPost = {setPost}
-        submitting = {submitting}
-        handleSubmit = {createPrompt}
-        />
-    )
-}
+  return (
+    <Form
+      type="Create"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={createPrompt}
+    />
+  );
+};
 
-export default CreatePrompt
+export default CreatePrompt;
